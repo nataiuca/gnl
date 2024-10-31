1f567580fa8b5c2478eee8d87177c84fc5602dde
nataiuca <movil.nataiuca@gmail.com>
2024-10-31 16:25:27 +0100

    Add files via upload

---
+++ nueva.c
@@ -0,0 +1,101 @@
+#include "get_next_line.h"
+
+static void handle_read_error(char **remainder)
+{
+    free(*remainder);
+    *remainder = NULL;
+}
+
+static char *append_buffer(char *remainder, char *buf)
+{
+    char    *temp;
+
+    if (!remainder)
+        remainder = ft_strdup(buf);
+    else
+    {
+        temp = ft_strjoin(remainder, buf);
+        free(remainder);
+        remainder = temp;
+    }
+    return (remainder);
+}
+
+static int read_and_store(int fd, char **remainder)
+{
+    char    buf[BUFFER_SIZE + 1];
+    int     read_bytes;
+
+    read_bytes = read(fd, buf, BUFFER_SIZE);
+    while (read_bytes > 0)
+    {
+        buf[read_bytes] = '\0';
+        *remainder = append_buffer(*remainder, buf);
+        if (!*remainder)
+            return (-1);
+        if (ft_strchr(*remainder, '\n'))
+            break ;
+        read_bytes = read(fd, buf, BUFFER_SIZE);
+    }
+    return (read_bytes);
+}
+
+static char *get_partial_line(char **remainder, char *temp)
+{
+    char    *line;
+    char    *new_remainder;
+    size_t  len;
+
+    len = temp - *remainder + 1;
+    line = ft_strndup(*remainder, len);
+    if (!line)
+        return (NULL);
+    new_remainder = ft_strdup(temp + 1);
+    if (!new_remainder)
+    {
+        free(line);
+        return (NULL);
+    }
+    free(*remainder);
+    *remainder = new_remainder;
+    if (**remainder == '\0')
+    {
+        free(*remainder);
+        *remainder = NULL;
+    }
+    return (line);
+}
+
+char *get_line(char **remainder)
+{
+    char    *temp;
+    char    *line;
+
+    if (!(*remainder))
+        return (NULL);
+    temp = ft_strchr(*remainder, '\n');
+    if (temp)
+        return (get_partial_line(remainder, temp));
+    line = ft_strdup(*remainder);
+    free(*remainder);
+    *remainder = NULL;
+    return (line);
+}
+
+char *get_next_line(int fd)
+{
+    static char *remainder;
+    int         read_bytes;
+
+    if (fd < 0 || BUFFER_SIZE <= 0)
+        return (NULL);
+    read_bytes = read_and_store(fd, &remainder);
+    if (read_bytes == -1)
+    {
+        handle_read_error(&remainder);
+        return (NULL);
+    }
+    if (read_bytes == 0 && !remainder)
+        return (NULL);
+    return (get_line(&remainder));
+}
